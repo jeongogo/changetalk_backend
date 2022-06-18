1bbe4cefe64280da67c304b373526cec7110e05c
jeongogo <chcgogo2@naver.com>
2022-06-18 20:09:02 +0900

    'static'

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const userRoutes = require('./src/routes/userRoutes');
-const chatRoutes = require('./src/routes/chatRoutes');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./src/routes/userRoutes");
+const chatRoutes = require("./src/routes/chatRoutes");
 const app = express();
-const socket = require('socket.io');
-require('dotenv').config();
+const path = require("path");
+const socket = require("socket.io");
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', userRoutes);
-app.use('/api/chat', chatRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 const { PORT, MONGO_URI } = process.env;
+
+const root = path.join(__dirname, "../", "changetalk_frontend", "build");
+
+app.use(express.static(root));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.resolve(root));
+});
 
 mongoose
   .connect(MONGO_URI)
   .then(() => {
-    console.log('Connect to MongoDB');
+    console.log("Connect to MongoDB");
   })
   .catch((e) => {
     console.error(e);
@@ -30,12 +39,12 @@ const server = app.listen(PORT, () => {
 
 const io = socket(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: "http://localhost:3000",
     credentials: true,
   },
 });
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
   socket.on("join_room", (data) => {
